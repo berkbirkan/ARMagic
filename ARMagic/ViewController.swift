c773b1aac49466bc1a099badf2daf1a6181befce
Berk <berk-birkan@hotmail.com>
2018-12-28 22:51:07 +0300

    Initial Commit

---
+++ ARMagic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARMagic
+//
+//  Created by berk birkan on 28.12.2018.
+//  Copyright © 2018 Turansoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
